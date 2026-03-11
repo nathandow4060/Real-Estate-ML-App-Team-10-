77e17f1dfa7192e2aa5a9016fc4f919ef4b8bb68
WD40Drinker <Mitchtvu@hotmail.com>
2026-03-10 22:24:14 -0500

    basic graph.js react

--- frontend/src/ui/App.tsx
+++ frontend/src/ui/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import {Chart as ChartJS} from "chart.js/auto"
+import {Line} from "react-chartjs-2";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +16,24 @@ function App() {
       </div>
       <h1>React</h1>
       <div className="card">
+
+        <Line data = {{
+        labels: ['1990', '1991', '1992', '1993', '1994', '1995', '1996'],
+          datasets: [{
+            label: 'Housing Prices (USD $)',
+            data: [18, 12, 6, 9, 12, 3, 9],
+            backgroundColor: [
+            'rgba(255, 26, 104, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 26, 104, 1)'
+            ],
+            borderWidth: 1
+          }]
+        }}
+        />
+
+
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
