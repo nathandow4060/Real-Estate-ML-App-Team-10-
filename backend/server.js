07f985adb74f58bcb872a02d619104018f8bed22
Nathan Dow <nathandow4060@gmail.com>
2026-03-18 17:17:18 -0500

    Made the app.listen come after the routes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const app = express()
 app.use(express.static('../frontend/public')) //displays index.html file in public folder to base url of site '/'
 app.use(express.json())
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
 
 // property routes
 const propRoutes = require('./routes/propertyRoutes')
@@ -14,3 +13,5 @@ app.use('/property',propRoutes)
 // property sales routes
 const propSalesRoutes = require('./routes/propertySalesRoutes')
 app.use('/property-sales',propSalesRoutes)
+
+app.listen(5000, () => {console.log("Server started on port 5000")}) //This should be last after routes
