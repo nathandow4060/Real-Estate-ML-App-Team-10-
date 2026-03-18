9c5f774fdb49124dd205d12f0b688bd89c40fb5f
WD40Drinker <Mitchtvu@hotmail.com>
2026-03-18 16:13:01 -0500

    setting up post requests

--- frontend/src/ui/assets/search.tsx
+++ frontend/src/ui/assets/search.tsx
@@ -1,25 +1,39 @@
 import React, { useState } from 'react'
 import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete'
-import '@geoapify/geocoder-autocomplete/styles/minimal.css'
- 
+import '@geoapify/geocoder-autocomplete/styles/round-borders-dark.css'
+/* 
+var request;
+
 const Search = () => {
  
   const onPlaceSelected = (feature) => {
-  console.log('Selected:', feature?.properties);
-};
+    console.log('Selected:', feature?.properties);
 
-const onSuggestionsChange = (list) => {
-  console.log('Suggestions:', list);
-};
+    if(feature?.properties.result_type === "building"){
+      request = {
+        type: feature.properties.result_type,
+        data:{
+        address: feature?.properties.address_line1,
+        city:feature?.properties.city,
+        zipcode: feature?.properties.postcode,
+        state: feature?.properties.state_code
+      }}
+    }
+    console.log(request)
+  };
+
+  const onSuggestionsChange = (list) => {
+    console.log('Suggestions:', list);
+  };
  
   return <GeoapifyContext apiKey="c56847c51cc54d77a23f9d4caed09c74">
       <GeoapifyGeocoderAutocomplete placeholder="Enter address here"
         lang={'en'}
-        limit={5}
+        limit={9}
         filterByPlace={"512b2c5d66fd2e52c0590f9fcfdb33d34440f00101f901a287020000000000c0020a"}
         placeSelect={onPlaceSelected}
         //suggestionsChange={onSuggestionsChange}
-        />
+      />
     </GeoapifyContext>
-}
-export default Search
+  }
+export default Search */
