f3c13f51d8e0bc3ddb73b5e3f75e584e3ddca1fd
Nathan Dow <nathandow4060@gmail.com>
2026-03-21 01:35:15 -0500

    Fixed SQL query

--- backend/controllers/propertySalesController.js
+++ backend/controllers/propertySalesController.js
@@ -35,9 +35,7 @@ exports.getCityPriceHistory = async (req, res, next) => {
             ORDER BY year ASC
         `, [city, state])
         res.json({ status: 'success', data: result.rows })
-    } catch (err) {
-        next(err)
-    }
+    } catch (err) { next(err) }
 }
 
 exports.getCountyPriceHistory = async (req, res, next) => {
@@ -50,14 +48,12 @@ exports.getCountyPriceHistory = async (req, res, next) => {
                 ROUND(AVG(ps.sale_amount)) AS avg_price
             FROM public."Property" AS p
             JOIN public."Property_Sale" AS ps ON p.pid = ps.property_id
-            WHERE p.city ILIKE $1 AND p.state ILIKE $2
+            WHERE p.zipcode = $1 AND p.state ILIKE $2
             GROUP BY year
             ORDER BY year ASC
         `, [zipcode, state])
         res.json({ status: 'success', data: result.rows })
-    } catch (err) {
-        next(err)
-    }
+    } catch (err) { next(err) }
 }
 
 exports.getStatePriceHistory = async (req, res, next) => {
@@ -69,14 +65,12 @@ exports.getStatePriceHistory = async (req, res, next) => {
                 ROUND(AVG(ps.sale_amount)) AS avg_price
             FROM public."Property" AS p
             JOIN public."Property_Sale" AS ps ON p.pid = ps.property_id
-            WHERE p.city ILIKE $1 AND p.state ILIKE $2
+            WHERE p.state ILIKE $1
             GROUP BY year
             ORDER BY year ASC
         `, [state])
         res.json({ status: 'success', data: result.rows })
-    } catch (err) {
-        next(err)
-    }
+    } catch (err) { next(err) }
 }
 
 module.exports = {
