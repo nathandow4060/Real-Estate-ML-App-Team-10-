6e582c9c939d032c6219a7102194407d39256d74
ethan <ethan.mcdonell@icloud.com>
2026-04-15 16:04:58 -0500

    Added router export to predictionsRoutes.js and fixed routes to be post routes.

--- backend/routes/predictionsRoutes.js
+++ backend/routes/predictionsRoutes.js
@@ -4,5 +4,6 @@ const ctrl = require('../controllers/predictionsController')
 const router = express.Router()
 
 router.post('/property-predictions', ctrl.getPropertyPrediction) //return most recent date-of-sale price prediction 
-router.get('/model-details', ctrl.getModelDetailsByName) //return model details for passed model name
-router.get('/model-perf', ctrl.getModelMetricsByName) //return model performance metrics for passed model name
+router.post('/model-details', ctrl.getModelDetailsByName) //return model details for passed model name
+router.post('/model-perf', ctrl.getModelMetricsByName) //return model performance metrics for passed model name
+module.exports = router
