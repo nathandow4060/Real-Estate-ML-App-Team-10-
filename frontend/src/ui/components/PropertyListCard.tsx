20df8de5c7ffd7fb4095280a84b6381bf54a9fbd
WD40Drinker <Mitchtvu@hotmail.com>
2026-04-16 00:38:57 -0500

    Update PropertyListCard.tsx

--- frontend/src/ui/components/PropertyListCard.tsx
+++ frontend/src/ui/components/PropertyListCard.tsx
@@ -8,13 +8,13 @@ interface Attribute {
 
 function PropertyListCard({attributes}: {attributes: any[]}){
 
-    const displayAttributes = attributes.slice(0,7)
+    //const displayAttributes = attributes.slice(0,7)
 
     return(
 
         <table className="attr-table">
                 <tbody>
-                  {displayAttributes.map((attr: Attribute, i: number) => (
+                  {attributes.map((attr: Attribute, i: number) => (
                     <tr key={i}>
                       <td className="attr-label">{attr.label}</td>
                       <td className="attr-value">{attr.value ?? '—'}</td>
