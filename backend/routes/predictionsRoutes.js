278576cde112bd3c4f94a913388989d0c0331eae
Henry Trahan <henrytrahan@protonmail.com>
2026-04-21 13:42:41 -0500

    Update predictionsRoutes.js

--- backend/routes/predictionsRoutes.js
+++ backend/routes/predictionsRoutes.js
@@ -3,7 +3,9 @@ const express = require('express')
 const ctrl = require('../controllers/predictionsController')
 const router = express.Router()
 
-router.post('/property-predictions', ctrl.getPropertyPrediction) //return most recent date-of-sale price prediction 
-router.post('/model-details', ctrl.getModelDetailsByName) //return model details for passed model name
-router.post('/model-perf', ctrl.getModelMetricsByName) //return model performance metrics for passed model name
+router.post('/property-predictions', ctrl.getPropertyPrediction)
+router.post('/model-details', ctrl.getModelDetailsByName)
+router.post('/model-perf', ctrl.getModelMetricsByName)
+router.post('/zipcode-averages', ctrl.getAveragePredictionsByZipcode)
+
 module.exports = router
