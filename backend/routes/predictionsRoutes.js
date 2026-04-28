61176e6db4b281bca6781d19908e754554d61db5
Henry Trahan <henrytrahan@protonmail.com>
2026-04-28 12:24:17 -0500

    Update predictionsRoutes.js

--- backend/routes/predictionsRoutes.js
+++ backend/routes/predictionsRoutes.js
@@ -8,5 +8,6 @@ router.post('/model-details', ctrl.getModelDetailsByName)
 router.post('/model-perf', ctrl.getModelMetricsByName)
 router.post('/zipcode-averages', ctrl.getAveragePredictionsByZipcode)
 router.post('/city-averages', ctrl.getAveragePredictionsByCity)
+router.post('/state-averages', ctrl.getAveragePredictionsByState)
 
 module.exports = router
