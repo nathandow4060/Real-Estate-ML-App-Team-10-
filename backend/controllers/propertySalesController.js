8732f253468cbbdc4af23889976e06a1f7a20624
Nathan Dow <nathandow4060@gmail.com>
2026-04-30 18:51:39 -0500

    Changed SQL in controller

--- backend/controllers/propertySalesController.js
+++ backend/controllers/propertySalesController.js
@@ -9,10 +9,12 @@ exports.getPropertySales = async (req, res, next) => {
         const state = req.body.state
 
         const result = await db.query(`
-                SELECT ps.date_of_sale, ps.sale_amount FROM public."Property" AS p JOIN
-                public."Property_Sale" AS ps on p.pid = ps.property_id
-                WHERE p.street_address = $1 AND p.zipcode = $2 AND p.city = $3 AND P.state = $4
-            `, [street_addr, zipcode, city, state])
+            SELECT ps.date_of_sale, ps.sale_amount 
+            FROM public."Property" AS p 
+            JOIN public."Property_Sale" AS ps ON p.pid = ps.property_id
+            WHERE p.street_address = $1 AND p.zipcode = $2 AND p.city = $3 AND p.state = $4
+            ORDER BY TO_DATE(ps.date_of_sale, 'MM/DD/YYYY') ASC
+        `, [street_addr, zipcode, city, state])
         res.json({ status: 'success', count: result.rowCount, data: result.rows})
     } catch (err) {
         next(err)
